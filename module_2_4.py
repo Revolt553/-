e9b64ad51e95148295750b3ed9afab9f4c61172f
Revolt553 <revolt553@mail.ru>
2024-05-26 19:37:10 +0400

    Add files via upload

--- module_2_4.py
+++ module_2_4.py
@@ -1,11 +1,13 @@
 numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
 primes = []
 not_primes = []
-for i in numbers:
+for i in numbers[1::]:
     count = 0
-    for j in range(1, i + 1):
+    for j in range(2, i):
         if i % j == 0 and i != j and j != 1:
             count += 1
+    if i == 0:
+        break
     if count == 0:
         primes.append(i)
     if count != 0:
