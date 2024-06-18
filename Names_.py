366dbe0e2664d8a39fe7d7bd77ec6bca1d899a71
Revolt553 <revolt553@mail.ru>
2024-06-18 15:35:24 +0400

    Add files via upload

--- Names_.py
+++ Names_.py
@@ -1,8 +1,5 @@
-a = str(123)
-def test_function(a):
-    x = a
-    def inner_function(a):
-        x = 'Я в области видимости функции test_function'
-        print(x)
-    inner_function(a)
-print(test_function(a))
+def test_function():
+    def inner_function():
+        print('Я в области видимости функции test_function')
+    inner_function()
+test_function()
