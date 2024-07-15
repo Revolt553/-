3ba812f6f1353a18d2c5b7bb8f73430bbfba681f
Revolt553 <revolt553@mail.ru>
2024-07-15 14:41:09 +0400

    Add files via upload

--- module_7_2.py
+++ module_7_2.py
@@ -1,11 +1,5 @@
 import io
 from pprint import pprint
-
-'''def custom_write(self, file_name, strings):
-    self.file_name = open(file_name, 'a', encoding='utf-8')
-    self.strings = strings[]'''
-
-
 def custom_write(file_name, strings):
     file1 = open(file_name, 'w')
     tell_1_pos = file1.tell()
@@ -18,25 +12,18 @@ def custom_write(file_name, strings):
     tell_4_pos = file1.tell()
     file1.write(f'{strings[3]}')
     file1 = open(file_name, 'r', encoding='utf-8')
-    # for index, line in enumerate(file1, 1): #попробовал получить номера строк
-    #     print(index)
     file1.close()
     keys = [(1, tell_1_pos), (2, tell_2_pos), (3, tell_3_pos), (4, tell_4_pos)]
     strings_position = dict(zip(keys, strings))
     return strings_position
 file_name = 'test.txt'
 strings = ['Text for tell.', 'Используйте кодировку utf-8.', 'Because there are 2 languages!',  'Спасибо!']
-
-result = custom_write('test.txt', strings)
-
-
 info = [
     'Text for tell.',
     'Используйте кодировку utf-8.',
     'Because there are 2 languages!',
     'Спасибо!'
     ]
-
-result = custom_write('test.txt', info)
+result = custom_write('test.txt', strings)
 for elem in result.items():
-  print(elem)
+    print(elem)
