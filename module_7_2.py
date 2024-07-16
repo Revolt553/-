5e90f3d54b5ca2876379e63d186315f99d783d7c
Revolt553 <revolt553@mail.ru>
2024-07-16 12:26:29 +0400

    Add files via upload

--- module_7_2.py
+++ module_7_2.py
@@ -1,29 +1,18 @@
-import io
-from pprint import pprint
 def custom_write(file_name, strings):
-    file1 = open(file_name, 'w')
-    tell_1_pos = file1.tell()
-    file1.write(f'{strings[0]}\n')
-    tell_2_pos = file1.tell()
-    file1 = open(file_name, 'a', encoding='utf-8')
-    file1.write(f'{strings[1]}\n')
-    tell_3_pos = file1.tell()
-    file1.write(f'{strings[2]}\n')
-    tell_4_pos = file1.tell()
-    file1.write(f'{strings[3]}')
-    file1 = open(file_name, 'r', encoding='utf-8')
-    file1.close()
-    keys = [(1, tell_1_pos), (2, tell_2_pos), (3, tell_3_pos), (4, tell_4_pos)]
-    strings_position = dict(zip(keys, strings))
+    file = open(file_name, 'w', encoding='utf-8')
+    strings_position = {}
+    for index, string in enumerate(strings, start=1):
+        file.write(string + '\n')
+        position = file.tell()
+        strings_position[(index, position)] = string
+    file.close()
     return strings_position
-file_name = 'test.txt'
-strings = ['Text for tell.', 'Используйте кодировку utf-8.', 'Because there are 2 languages!',  'Спасибо!']
 info = [
     'Text for tell.',
     'Используйте кодировку utf-8.',
     'Because there are 2 languages!',
     'Спасибо!'
     ]
-result = custom_write('test.txt', strings)
+result = custom_write('test.txt', info)
 for elem in result.items():
     print(elem)
