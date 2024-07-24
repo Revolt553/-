0dcf67af8476cb351a4445ac5a1d413eea1c52ed
Revolt553 <revolt553@mail.ru>
2024-07-25 00:00:59 +0400

    Add files via upload

---
+++ module_8_1.py
@@ -0,0 +1,11 @@
+def add_everything_up(a, b):
+    try:
+        a + b == int
+    except:
+        return (str(a) + str(b))
+    else:
+        return (round(a + b, 3))
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
